30194815e362b25a3c02e2ec5f97baef3270134a
Mati Añaños <mananos@frba.utn.edu.ar>
2022-10-26 00:19:51 -0300

    Agregamos controller de delegacion - No se ve xd

--- RelacionamientoPersonas/src/main/java/relacionamiento/presentacion/DelegacionController.java
+++ RelacionamientoPersonas/src/main/java/relacionamiento/presentacion/DelegacionController.java
@@ -1,9 +1,42 @@
 package relacionamiento.presentacion;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import relacionamiento.domain.Persona;
+import relacionamiento.domain.RepoPersonas;
 
-@RestController
-@CrossOrigin
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
 public class DelegacionController {
+
+    private final Handlebars handlebars;
+
+    public DelegacionController() {
+        this.handlebars = new Handlebars();
+
+    }
+
+    @GetMapping(value = "/delegaciones", produces = MediaType.TEXT_HTML_VALUE) //-> importante en Spring
+    //public ResponseEntity<String> obtenerVistaDeTodas(@RequestParam("sesion") String idSesion) throws IOException {
+    public ResponseEntity<String> obtenerVistaDeTodasDelegaciones() throws IOException {
+        //validar accion en capa modelo según roles o usuario asociados al idSesion
+        Template template = handlebars.compile("/templates/Plantillas/listadelegaciones");
+        List<Persona> personas = RepoPersonas.getPersonas();
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("listadelegaciones", personas);
+
+        String html = template.apply(model);
+
+        return ResponseEntity.status(200).body(html);
+
+    }
 }
--- RelacionamientoPersonas/src/main/java/relacionamiento/presentacion/PersonaController.java
+++ RelacionamientoPersonas/src/main/java/relacionamiento/presentacion/PersonaController.java
@@ -4,15 +4,11 @@ import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
 import relacionamiento.domain.Persona;
 import relacionamiento.domain.RepoPersonas;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -23,9 +19,10 @@ import java.util.Map;
 public class PersonaController {
 
  // private final RepoPersonas repoPersonas;
- private final Handlebars handlebars;
+  private final Handlebars handlebars;
  public PersonaController() {
-  this.handlebars = new Handlebars();
+    this.handlebars = new Handlebars();
+
  }
 
 //    Rest API controller:
@@ -36,10 +33,10 @@ public class PersonaController {
 //    }
 
  @GetMapping(value = "/personas", produces = MediaType.TEXT_HTML_VALUE) //-> importante en Spring
- public ResponseEntity<String> obtenerVistaDeTodas(@RequestParam("sesion") String idSesion) throws IOException {
-
+ //public ResponseEntity<String> obtenerVistaDeTodas(@RequestParam("sesion") String idSesion) throws IOException {
+ public ResponseEntity<String> obtenerVistaDeTodas() throws IOException {
   //validar accion en capa modelo según roles o usuario asociados al idSesion
-  Template template = handlebars.compile("/templates/TempladePersoRegistradas/listapersonas");
+  Template template = handlebars.compile("/templates/Plantillas/listapersonas");
   List<Persona> personas = RepoPersonas.getPersonas();
 
   Map<String, Object> model = new HashMap<>();
