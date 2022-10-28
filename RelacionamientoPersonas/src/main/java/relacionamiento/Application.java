8ef2f71effe24497aa73b667f211acc772050def
Rodri-Putrino <rputrino@frba.utn.edu.ar>
2022-10-28 02:03:09 -0300

    Agregamos carga de json
    Co-authored-by: IgnacioG2000 <62635818+IgnacioG2000@users.noreply.github.com>
    Co-authored-by: mercedesmagnelli <62451237+mercedesmagnelli@users.noreply.github.com>

--- RelacionamientoPersonas/src/main/java/relacionamiento/Application.java
+++ RelacionamientoPersonas/src/main/java/relacionamiento/Application.java
@@ -1,20 +1,22 @@
 package relacionamiento;
 
+import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import relacionamiento.domain.PersonaJson;
+import relacionamiento.domain.RepoPersonaJson;
 
 import java.io.IOException;
 
 @SpringBootApplication
 public class Application{
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, ParseException {
     SpringApplication.run(Application.class, args);
 
-    PersonaJson personita = new PersonaJson("aa", " ", "");
+    RepoPersonaJson r = new RepoPersonaJson();
+    r.cargarRepoPersonas();
 
-    personita.imprimirListaDePersonasJson();
   }
 
 }
--- RelacionamientoPersonas/src/main/java/relacionamiento/domain/PersonaJson.java
+++ RelacionamientoPersonas/src/main/java/relacionamiento/domain/PersonaJson.java
@@ -1,39 +1,17 @@
 package relacionamiento.domain;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 public class PersonaJson {
-    private String dni;
+
     private String nombre;
     private String apellido;
-
-    Gson gson = new GsonBuilder()
-            .setLenient()
-            .create();
-
-    String jsonPrueba =
-    "{'data':[{ 'Nombre': 'Ignacio',"
-            +  "'Apellido': 'askjsa',"
-            +  "'DNI': '11111111'},"
-            + "{'Nombre': 'Mercedes',"
-            +  "'Apellido': 'askjsa',"
-            +   "'DNI': '22222222'}]}";
+    private String dni;
 
     public PersonaJson(String dni, String nombre, String apellido) {
-        this.dni = dni;
         this.nombre = nombre;
         this.apellido = apellido;
+        this.dni = dni;
     }
 
-    PersonaJson[] listaDePersonas = gson.fromJson(jsonPrueba, PersonaJson[].class);
-
-    public void imprimirListaDePersonasJson(){
-
-        for(PersonaJson persona : listaDePersonas) {
-            System.out.println(persona);
-        }
-    }
     public String getDni() {
         return dni;
     }
