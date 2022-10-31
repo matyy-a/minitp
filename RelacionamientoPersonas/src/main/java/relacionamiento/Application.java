94e0aa512ccb8090a55aff960a72aeec6f86e2cc
mercedesmagnelli <mechamagnelli@gmail.com>
2022-10-31 00:54:39 -0300

    agregamosEventListeners
    Co-authored-by: Rodri-Putrino <81584373+Rodri-Putrino@users.noreply.github.com>

--- RelacionamientoPersonas/src/main/java/relacionamiento/Application.java
+++ RelacionamientoPersonas/src/main/java/relacionamiento/Application.java
@@ -3,8 +3,6 @@ package relacionamiento;
 import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import relacionamiento.domain.PersonaJson;
-import relacionamiento.domain.RepoPersonaJson;
 
 import java.io.IOException;
 
